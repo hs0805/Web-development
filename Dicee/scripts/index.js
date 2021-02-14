8714483d40e87a062fcc71798a212c6b14623b5f
Happy Singh <hsingh0805@gmail.com>
2021-02-14 20:10:00 +0530

    Dicee challange completed

---
+++ Dicee/scripts/index.js
@@ -0,0 +1,18 @@
+alert("working")
+
+let num1 = Math.floor(Math.random() * 6) + 1;
+let num2 = Math.floor(Math.random() * 6) + 1;
+
+var imageSrc = "images/dice"+num1+".png";
+document.querySelector(".img1").setAttribute("src", imageSrc);
+
+imageSrc = "images/dice"+num2+".png";
+document.querySelector(".img2").setAttribute("src", imageSrc);
+
+
+if (num1>num2){
+    document.querySelector("h1").textContent = "Player1 Won";
+}
+else {
+    document.querySelector("h1").textContent = "Player2 Won";
+}
